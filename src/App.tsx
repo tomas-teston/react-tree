dc4137d1e059276519b6af7a00bc40795e37574e
tmunoz <tmunoz@plainconcepts.com>
2023-06-08 10:35:32 +0200

    example vtree

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,195 @@
-import { DATA, DATA_LAZY } from "./constants/constants";
-import { TreeNodeModel, TreeNodeTemplateProps } from "./components/tree/tree-node";
+import AutoSizer, { VerticalSize } from 'react-virtualized-auto-sizer';
+import React, { FC, useCallback, useMemo, useRef, useState } from 'react';
 
-import Tree from "./components/tree/tree";
+import { AsyncTaskScheduler } from './utils/utils';
+import { FixedSizeTree } from 'react-vtree';
 
-const TreeNodeComponent = ({ node, nestingLevel, loading, selected, onToogle, onSelected }: TreeNodeTemplateProps) => {
-  return (
-    <div
-      style={{ cursor: "pointer", marginLeft: nestingLevel * 16, backgroundColor: selected ? 'lightblue' : 'white' }}>
-      {loading ?
-        <span>Loading...</span> :
-        <div style={{ display: 'flex' }} onClick={() => {
-          onSelected?.();
-          onToogle?.();
-        }}>
-          <span>
-            {node.label}
-          </span>
-          <div>
-            ✨
-          </div>
-        </div>
-      }
-    </div>);
-};
+document.body.style.margin = '0';
+document.body.style.display = 'flex';
+document.body.style.minHeight = '100vh';
 
-const App = () => {
-  const handleOnExpand = (nodeEvent: TreeNodeModel) => {
-    console.info("load children for node with id: ", nodeEvent.id)
-    return new Promise<TreeNodeModel[]>((res) => setTimeout(() => res(DATA_LAZY), 2000));
+const root = document.getElementById('root')!;
+root.style.margin = '10px 0 0 10px';
+root.style.flex = '1';
+
+type TreeNode = Readonly<{
+  children: TreeNode[];
+  downloaded: boolean;
+  id: number;
+  name: string;
+}>;
+
+type TreeData = any &
+  Readonly<{
+    downloaded: boolean;
+    download: () => Promise<void>;
+    isLeaf: boolean;
+    name: string;
+    nestingLevel: number;
+  }>;
+
+let nodeId = 0;
+
+const createNode = (
+  downloadedIds: readonly number[],
+  depth: number = 0,
+): TreeNode => {
+  const id = nodeId;
+  const node: TreeNode = {
+    children: [],
+    downloaded: downloadedIds.includes(id),
+    id,
+    name: `test-${nodeId}`,
+  };
+
+  nodeId += 1;
+
+  if (depth === 5) {
+    return node;
   }
 
+  for (let i = 0; i < 10; i++) {
+    node.children.push(createNode(downloadedIds, depth + 1));
+  }
+
+  return node;
+};
+
+const defaultTextStyle = { marginLeft: 10 };
+const defaultButtonStyle = { fontFamily: 'Courier New' };
+
+type NodeMeta = Readonly<{
+  nestingLevel: number;
+  node: TreeNode;
+}>;
+
+const getNodeData = (
+  node: TreeNode,
+  nestingLevel: number,
+  download: () => Promise<void>,
+): any => ({
+  data: {
+    download,
+    downloaded: node.downloaded,
+    id: node.id.toString(),
+    isLeaf: node.children.length === 0,
+    isOpenByDefault: false,
+    name: node.name,
+    nestingLevel,
+  },
+  nestingLevel,
+  node,
+});
+
+const Node = ({
+  data: { download, downloaded, isLeaf, name, nestingLevel },
+  isOpen,
+  style,
+  setOpen,
+}: any) => {
+  const [isLoading, setLoading] = useState(false);
+
   return (
-    <div className="App">
-      <Tree nodes={DATA} nodeTemplate={TreeNodeComponent} loadMoreNodes={handleOnExpand} />
+    <div
+      style={{
+        ...style,
+        alignItems: 'center',
+        display: 'flex',
+        marginLeft: nestingLevel * 30 + (isLeaf ? 48 : 0),
+      }}
+    >
+      {!isLeaf && (
+        <div>
+          <button
+            type="button"
+            onClick={async () => {
+              if (!downloaded) {
+                setLoading(true);
+                await download();
+                await setOpen(!isOpen);
+                setLoading(false);
+              } else {
+                await setOpen(!isOpen);
+              }
+            }}
+            style={defaultButtonStyle}
+          >
+            {isLoading ? '⌛' : isOpen ? '-' : '+'}
+          </button>
+        </div>
+      )}
+      <div style={defaultTextStyle}>{name}</div>
     </div>
   );
+};
+
+type TreePresenterProps = Readonly<{
+  disableAsync: boolean;
+  itemSize: number;
+}>;
+
+const App = () => {
+  const [downloadedIds, setDownloadedIds] = useState<readonly number[]>([]);
+  const scheduler = useRef<AsyncTaskScheduler<number>>(
+    new AsyncTaskScheduler((ids: any) => {
+      setDownloadedIds(ids);
+    }),
+  );
+
+  const rootNode = useMemo(() => {
+    nodeId = 0;
+
+    return createNode(downloadedIds);
+  }, [downloadedIds]);
+
+  const createDownloader = (node: TreeNode) => (): Promise<void> =>
+    new Promise((resolve) => {
+      const timeoutId = setTimeout(() => {
+        scheduler.current.finalize();
+      }, 2000);
+
+      scheduler.current.add(node.id, resolve, () => clearTimeout(timeoutId));
+    });
+
+  const treeWalker = useCallback(
+    function* treeWalker(): any {
+      yield getNodeData(rootNode, 0, createDownloader(rootNode));
+
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      while (true) {
+        const parentMeta = yield;
+
+        if (parentMeta.data.downloaded) {
+          // eslint-disable-next-line @typescript-eslint/prefer-for-of
+          for (let i = 0; i < parentMeta.node.children.length; i++) {
+            yield getNodeData(
+              parentMeta.node.children[i],
+              parentMeta.nestingLevel + 1,
+              createDownloader(parentMeta.node.children[i]),
+            );
+          }
+        }
+      }
+    },
+    [rootNode],
+  );
+
+  return (
+    <AutoSizer disableWidth>
+      {({ height }: VerticalSize) => (
+        <FixedSizeTree
+          treeWalker={treeWalker}
+          itemSize={30}
+          height={height}
+          async={true}
+          width="100%"
+        >
+          {Node}
+        </FixedSizeTree>
+      )}
+    </AutoSizer>
+  );
+
 }
 
 export default App;
